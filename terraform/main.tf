7e45e7637b87a1225e947fe76b84bf1b6f29d947
abhishek123-cmyk <134828971+abhishek123-cmyk@users.noreply.github.com>
2026-01-03 19:06:59 +0530

    Update Terraform configuration for ECR module
    Removed GitHub Actions workflow for Terraform EC2 deployment and added ECR module configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,7 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
 }
+
 module "ecr" {
   source = "../../modules/ecr"
 
@@ -28,44 +29,9 @@ module "ecr" {
     "order-service",
     "payment-service",
     "inventory-service",
+    "inventory-service",
     "notification-service",
     "gateway-service"
   ]
 }
-name: Terraform EC2 Deployment
 
-on:
-  push:
-    branches:
-      - main
-
-jobs:
-  terraform:
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v4
-
-      - name: Setup Terraform
-        uses: hashicorp/setup-terraform@v3
-
-      - name: Configure AWS credentials
-        uses: aws-actions/configure-aws-credentials@v4
-        with:
-          aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
-          aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-          aws-region: ${{ secrets.AWS_REGION }}
-
-
-      - name: Terraform Init
-        run: terraform init
-        working-directory: ec2-creation-in-us-east-1
-
-      - name: Terraform Plan
-        run: terraform plan
-        working-directory: ec2-creation-in-us-east-1
-
-      - name: Terraform Apply
-        run: terraform apply -auto-approve
-        working-directory: ec2-creation-in-us-east-1
